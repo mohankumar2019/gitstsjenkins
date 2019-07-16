3147b6fb1fd6034349ce8242f546f087f1c9aff9
mohan <mohan.chanti4@gmail.com>
2019-07-16 15:43:48 +0530

    changed file

---
+++ src/main/java/org/com/app/gitmav/App.java
@@ -0,0 +1,13 @@
+package org.com.app.gitmav;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World1111112222!" );
+    }
+}
